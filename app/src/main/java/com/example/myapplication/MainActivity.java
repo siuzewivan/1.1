f95633e834bed55b505c0022845f865674e09e2f
siuzewivan <74261217+siuzewivan@users.noreply.github.com>
2021-03-13 21:01:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = (TextView)findViewById(R.id.textView);
+        Log.d("MainActivity", "Hello World");
+    }
+}
